56f8b748ac21d849d3e998a9023ff300c90351b5
Peter Marheine <peter@taricorp.net>
2019-08-21 11:13:14 +1000

    Some actual deduplication, a disclaimer

--- README.md
+++ README.md
@@ -4,8 +4,21 @@ Given an input WARC file, output a copy with duplicate response records
 rewritten to be revisit records pointing to the first instance of that response
 in the same file.
 
+Disclaimer: this tool doesn't currently do much that can be considered useful;
+it's both a prototype and a work in progress.
+
 ## Future improvements
 
  * More speed. Faster decompression (flate2 is faster than libflate for
    decompression), parallel reading/parsing with some kind of exotic buffer
    (or possibly iobuf).
+
+ * Something about interop with warcio the python module.
+
+ * Check behavior around <> surrounding some URIs:
+   github.com/iipc/warc-specifications/pull/24
+
+ * Per above, handle all of
+   * Correctly formed WARC 1.0
+   * Malformed URIs in WARC 1.1
+   * Correctly formed WARC 1.1
--- src/bin/warcdedupe.rs
+++ src/bin/warcdedupe.rs
@@ -12,6 +12,7 @@ extern crate warcio;
 use docopt::Docopt;
 use libflate::gzip;
 use sha1::{Digest, Sha1};
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read, Write};
 use std::path::{Path, PathBuf};
@@ -109,6 +110,9 @@ fn main() {
     let mut input = open_input_stream(args.arg_infile, args.flag_compressed_input);
     let mut output = open_output_stream(args.arg_outfile, args.flag_compress_output);
 
+    let mut input_size = 0u64;
+    let mut output_size = 0u64;
+    let mut dedup = InMemoryResponseLog::new();
     loop {
         let mut record = match warcio::reader::Record::read_from(&mut input) {
             Ok(r) => r,
@@ -142,14 +146,111 @@ fn main() {
             };
             sha.input(&buf[..n]);
         }
-        let digest = sha.result();
+        let digest = {
+            let mut fixed = [0u8; 20];
+            fixed.copy_from_slice(&sha.result());
+            fixed
+        };
 
-        let target = record.header.field_str("warc-target-uri")
-            .unwrap_or("<UNKNOWN>");
+        let target = match record.header.field_str("warc-target-uri") {
+            None => {
+                eprintln!("Ignoring record without warc-target-uri: {:?}", record.header);
+                continue;
+            },
+            Some(t) => t
+        };
+        let timestamp = match record.header.warc_date() {
+            None => {
+                eprintln!("Ignoring record without warc-date: {:?}", record.header);
+                continue;
+            },
+            Some(t) => t,
+        };
 
-        for byte in digest {
-            print!("{:02X}", byte);
+        // TODO need to look at HTTP response body.
+        // Response headers tend to vary (Date: headers..), so check the response
+        // Content-Type (application/http) and try to look at only the response
+        // body. In case of match, still write out the HTTP headers since we haven't
+        // deduplicated them.
+        //
+        // Also need to add WARC-Refers-To to the new revisit record with the WARC-Record-ID
+        // of the original copy, where the original is always the chronologically earliest.
+        //
+        // We can parallelize (and possibly lose some savings) by recording the first-seen
+        // date and retrieving it back out, updating it if the current record is older than
+        // the currently-known oldest record. If the returned timestamp is earlier than
+        // the current record, write a revisit pointing at the returned record ID.
+        //
+        // Parallelism requires either multiple files input, or a work queue that might be
+        // derived from a cdx index or another worker just seeking through records and recording
+        // their start points in a larger file.
+        input_size += len;
+        match dedup.add(target.to_owned(), timestamp.to_owned(), digest) {
+            Some((_, first_seen)) => {
+                println!("{} dup {} size {}", target, first_seen, len);
+            },
+            None => {
+                // Not a duplicate
+                output_size += len;
+            }
         }
-        println!(" {} {}", len, target);
     }
+
+    eprintln!("{} bytes in, {} out\n{} saved", input_size, output_size, input_size - output_size);
+}
+
+/// Generic log for tracking seen responses.
+///
+/// While the basic version of this is an in-memory mapping, for very large
+/// archives an alternate index may be desired.
+trait ResponseLog {
+    /// Record a response for the specified URI with SHA-1 digest, captured at
+    /// the specified date.
+    ///
+    /// Returns None if that response has not been seen before, otherwise the
+    /// URI and date that it was first seen.
+    // TODO arbitrary GenericArray digest that could be multiple ones
+    // concatenated for better collision resistance?
+    fn add<'a, 'b>(&'a mut self, target_uri: String, date: String, digest: [u8; 20])
+        -> Option<(String, &'a str)>;
+}
+
+/// Basic implementation of a `ResponseLog`.
+///
+/// This implementation will only deduplicate identical responses from the same
+/// URL because it currently lacks strong protection against hash collisions.
+struct InMemoryResponseLog(HashMap<([u8; 20], String), String>);
+
+impl InMemoryResponseLog {
+    fn new() -> Self {
+        InMemoryResponseLog(
+            HashMap::new()
+        )
+    }
+}
+
+impl ResponseLog for InMemoryResponseLog {
+    fn add<'a, 'b>(&'a mut self, target_uri: String, date: String, digest: [u8; 20]) -> Option<(String, &'a str)> {
+        // API limitation: we need to take an owned copy of the target URI to look up, even if we
+        // don't need to insert since K: Borrow<Q> doesn't allow us to return a ref to a tuple that
+        // the HashMap owns. But we also can't get a ref to the key to return without
+        // HashMap::get_key_value (#49347), so we can at least reuse it.
+        let key = (digest, target_uri.to_owned());
+        if self.0.contains_key(&key) {
+            let first_seen = self.0.get(&key).unwrap();
+            Some((key.1, first_seen))
+        } else {
+            self.0.insert(key, date);
+            None
+        }
+    }
+}
+
+/// A `ResponseLog` implementation backed by LMDB, a high-performance embedded key-value store.
+/// 
+/// LMDB should scale better than the simple in-memory response log and is persistent, but like
+/// the in-memory log is limited to a single machine.
+#[cfg(feature = "lmdb")]
+struct LmdbResponseLog {
+
 }
